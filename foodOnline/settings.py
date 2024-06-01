e78206dbe5557e18eb781213f31a4aa479fb8fa7
ishwar patgar <ishwarpatgar2000@gmail.com>
2024-06-01 20:09:00 +0530

    new

--- foodOnline/settings.py
+++ foodOnline/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cw#*hsfmgy-*^xhwyt=453-weypwl%ty_=ko!kpv=l62l*n$bi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = [".vercel.app", ".now.sh", "127.0.0.1", "localhost"]
 
 
 # Application definition
