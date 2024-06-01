0473085ee80bac62f0fa5292eae6c82569d5726a
ishwar patgar <ishwarpatgar2000@gmail.com>
2024-06-01 19:54:10 +0530

    new

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
-python3.9 -m pip install --upgrade pip
+
 # create a virtual environment named 'venv' if it doesn't already exist
 python3.9 -m venv venv
 
@@ -7,6 +7,8 @@ python3.9 -m venv venv
 source venv/bin/activate
 
 # install all deps in the venv
+python3.9 -m pip install --upgrade pip
+
 pip install -r requirements.txt
 
 # collect static files using the Python interpreter from venv
