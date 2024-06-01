0b851d867cd7fb61f1c27b0b8a63db3d545c7b86
ishwar patgar <ishwarpatgar2000@gmail.com>
2024-06-01 17:16:31 +0530

    new

--- foodOnline/foodOnline/settings.py
+++ foodOnline/foodOnline/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
